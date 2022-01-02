b83f2e364d0de943ac0b39f6ba22f5b237902cbf
numeru <numeru@naver.com>
2022-01-02 19:27:23 +0900

    feat : passport를 이용한 인증 api 추가

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const passport = require('passport');
 const db = require('./models');
+const cors = require('cors');
 const keywordRoute = require('./routes/keyword');
+const userRoute = require('./routes/user');
+const passportConfig = require('./passport');
 
 dotenv.config();
+
+const app = express();
 
 db.sequelize
   .sync()
@@ -12,8 +20,31 @@ db.sequelize
   })
   .catch(console.error);
 
-const app = express();
+passportConfig();
+
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  session({
+    saveUninitialized: false,
+    resave: false,
+    secret: process.env.COOKIE_SECRET,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/keyword', keywordRoute);
+app.use('/user', userRoute);
 
-app.listen(3065);
+app.listen(3065, () => {
+  console.log('서버 실행 중');
+});
---
+++ routes/user.js
@@ -0,0 +1,124 @@
+const express = require('express');
+const { User, Keyword } = require('../models');
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
+
+const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+  try {
+    if (req.user) {
+      const fullUserWithoutPassword = await User.findOne({
+        where: { id: req.user.id },
+        attributes: {
+          exclude: ['password'],
+        },
+        include: [
+          {
+            model: Keyword,
+            attributes: ['content'],
+          },
+        ],
+      });
+      res.status(200).json({
+        statusCode: 200,
+        status: 'success',
+        message: '',
+        data: fullUserWithoutPassword,
+      });
+    } else {
+      res.status(200).json({
+        statusCode: 200,
+        status: 'success',
+        message: '',
+        data: null,
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.post('/login', isNotLoggedIn, (req, res, next) => {
+  passport.authenticate('local', (err, user, info) => {
+    if (err) {
+      console.error(err);
+      return next(err);
+    }
+    if (info) {
+      return res.status(401).send({
+        statusCode: 401,
+        status: 'fail',
+        message: info.reason,
+      });
+    }
+    return req.login(user, async (loginErr) => {
+      if (loginErr) {
+        console.error(loginErr);
+        return next(loginErr);
+      }
+      const fullUserWithoutPassword = await User.findOne({
+        where: { id: user.id },
+        attributes: {
+          exclude: ['password'],
+        },
+        include: [
+          {
+            model: Keyword,
+            attributes: ['content'],
+          },
+        ],
+      });
+      return res.status(200).json({
+        statusCode: 200,
+        status: 'success',
+        message: '로그인에 성공했습니다.',
+        data: fullUserWithoutPassword,
+      });
+    });
+  })(req, res, next);
+});
+
+router.post('/signup', isNotLoggedIn, async (req, res, next) => {
+  try {
+    const existUser = await User.findOne({
+      where: {
+        userId: req.body.userId,
+      },
+    });
+    if (existUser) {
+      return res.status(403).send({
+        statusCode: 403,
+        status: 'fail',
+        message: '이미 사용 중인 아이디입니다.',
+      });
+    }
+    const hashedPassword = await bcrypt.hash(req.body.password, 12);
+    await User.create({
+      userId: req.body.userId,
+      password: hashedPassword,
+    });
+    res.status(201).send({
+      statusCode: 201,
+      status: 'success',
+      message: '회원가입에 성공했습니다.',
+    });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.post('/logout', isLoggedIn, (req, res) => {
+  req.logout();
+  req.session.destroy();
+  res.status(200).send({
+    statusCode: 200,
+    state: 'success',
+    message: '로그아웃에 성공했습니다.',
+  });
+});
+
+module.exports = router;
