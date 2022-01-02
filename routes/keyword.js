24d5a3666840cf59d18134d21dd6dc520e126497
numeru <numeru@naver.com>
2022-01-02 19:27:02 +0900

    feat : 검색 기록 저장 api 추가

--- routes/keyword.js
+++ routes/keyword.js
@@ -1,5 +1,24 @@
 const express = require('express');
+const Keyword = require('../models/keyword');
+const { isLoggedIn } = require('./middlewares');
 
 const router = express.Router();
+
+router.post('/', isLoggedIn, async (req, res, next) => {
+  try {
+    const keyword = await Keyword.create({
+      content: req.body.content,
+    });
+    res.status(201).json({
+      statusCode: 201,
+      status: 'success',
+      message: '',
+      data: keyword,
+    });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
 
 module.exports = router;
